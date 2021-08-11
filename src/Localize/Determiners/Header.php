95682447c6c347e43c6ece9144f94ef991e6f585
Vince <38945597+vheubach@users.noreply.github.com>
2021-08-11 13:26:35 +0200

    Add support for header significance

--- src/Localize/Determiners/Header.php
+++ src/Localize/Determiners/Header.php
@@ -35,6 +35,64 @@ class Header extends Determiner
      */
     public function determineLocale(Request $request)
     {
-        return $request->header($this->header, $this->fallback);
+        $header = $request->header($this->header, $this->fallback);
+
+        $acceptLanguages = $this->sortBySignificance($header);
+
+        return $this->getSupported(array_keys($acceptLanguages));
+    }
+
+    /**
+     * Extract locales from header and sort them by significance.
+     *
+     * @param string $header
+     *
+     * @return array
+     */
+    public function sortBySignificance(string $header): array
+    {
+        $accepts = explode(',', $header);
+        $acceptLanguages = [];
+
+        foreach ($accepts as $accept) {
+            $locale = preg_replace('/([^;]+);.*$/', '${1}', $accept);
+
+            $significance = preg_replace('/^[^q]*q=([^\,]+)*$/', '${1}', $accept);
+
+            // The quality value defaults to "q=1"
+            $significance = is_numeric($significance) ? (float) $significance : 1.0;
+
+            $acceptLanguages[$locale] = $significance;
+        }
+
+        // Place higher-value on top
+        array_multisort($acceptLanguages, SORT_DESC, SORT_NATURAL);
+
+        return $acceptLanguages;
+    }
+
+    /**
+     * Pick up the first supported language.
+     *
+     * @param array $acceptedLanguages
+     *
+     * @return string
+     */
+    public function getSupported(array $acceptedLanguages): ?string
+    {
+        $supportedLocales = locales()->pluck('id');
+
+        foreach ($acceptedLanguages as $accepted) {
+            // Turn en-gb into en
+            $accepted = mb_substr($accepted, 0, 2);
+
+            foreach ($supportedLocales as $supported) {
+                if ($accepted === $supported) {
+                    return $supported;
+                }
+            }
+        }
+
+        return null;
     }
 }
